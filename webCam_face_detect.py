92ae94d3f1f7017b88011f8919a1e58f1a7cebf3
Dingchao Zhang <dingchaozhang@g.harvard.edu>
2017-05-03 22:52:21 -0500

    added line to append text to face square

--- webCam_face_detect.py
+++ webCam_face_detect.py
@@ -25,9 +25,13 @@ while True:
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+    	cv2.putText(frame,'Male,28',(x+w, y+h), font, 0.5,(0,255,0),1,cv2.LINE_AA)
+    	#out.write(frame)
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
+    
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
