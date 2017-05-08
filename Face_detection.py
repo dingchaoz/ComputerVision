fe9ba8d8e7e980eb9a01d1fa1d65f58f1ba8fbfe
Dingchao Zhang <dingchaozhang@g.harvard.edu>
2017-05-08 08:00:52 -0500

    modified face detection script taking image argument

--- Face_detection.py
+++ Face_detection.py
@@ -3,38 +3,32 @@ import sys
 import time
 import numpy as np
 import datetime
-# import keras
-# from keras.models import load_model
+
 from predict_gender import *
+from PIL import Image
 
 # Load the pre-trained face and eye classifier xml file, which are stored in opencv/data/haarcascades/folder
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-numSavedImgs = 0
-modelPath = 'saved_model_20170504.h5'
-strings = time.strftime("%Y,%m,%d,%H,%M,%S")
-newpath = strings.replace(',','') + '/'
-os.makedirs(newpath)
-modelRan = -1
-numFaces = 0
-font = cv2.FONT_HERSHEY_SIMPLEX
+
+"""
+Usage: python Face_detection.py trump_melania.jpg
+"""
 
 
-def read2Gray(imgPath = 'trump_melania.jpg'):
+def read2Gray(imgPath = 'selfie3.png'):
 	# Read image and make it in grayscale mode
 	img = cv2.imread(imgPath)
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)	
 	return img,gray
 
 
-def detectFaces(gray,scaleFactor = 1.1,minNeighbors=5,minSize=(30,30)):
+def detectFaces(face_cascade,gray,scaleFactor = 1.1,minNeighbors=5,minSize=(30,30)):
 	faces = face_cascade.detectMultiScale(gray, scaleFactor=scaleFactor,
         minNeighbors=minNeighbors,
         minSize=minSize)
 	return faces
 
 
-def saveFaceImg(x,y,w,h,numFaces):
+def saveFaceImg(x,y,w,h,numFaces,img,newpath):
 	margin = max(w,h)/2
 	cropFace = img[y-margin:y+h+margin,x-margin:x+w+margin]
 	saveFName = newpath+'roi'+str(numFaces)+'.png'
@@ -42,48 +36,54 @@ def saveFaceImg(x,y,w,h,numFaces):
 	print ('saved face img',numFaces)
 	return cropFace,saveFName
 
-def estSex(saveFName):
+def estSex(saveFName,model):
 	# resized_img = resize(cropFace)
 	# arr = im2Array(resized_img)
 	arr = np.array([loadImg2Array(saveFName)])
 	sex = predict_vgg(model,arr)
 	return sex
 
-def labelFaces(x,y,w,h):
+def labelFaces(x,y,w,h,img,sex,font):
 	cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 	imgLabel = sex
 	cv2.putText(img,imgLabel,(x, y), font, 1,(0,255,0),1,cv2.LINE_AA)
 
-model = loadVGG()
-img,gray = read2Gray()
-faces = detectFaces(gray)
+
+def main(argv):
 
 
+	face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+	numSavedImgs = 0
+	modelPath = 'saved_model_20170504.h5'
+	strings = time.strftime("%Y,%m,%d,%H,%M,%S")
+	newpath = strings.replace(',','') + '/'
+	os.makedirs(newpath)
+	modelRan = -1
+	numFaces = 0
+	font = cv2.FONT_HERSHEY_SIMPLEX
 
-for (x,y,w,h) in faces:
-	numFaces += 1
-	cropFace,saveFName = saveFaceImg(x,y,w,h,numFaces)
-	sex = estSex(saveFName)
-	labelFaces(x,y,w,h)
-	
-
-	
-    # eyes = eye_cascade.detectMultiScale(roi_gray)
-    # for (ex,ey,ew,eh) in eyes:
-    #     cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
-    #cv2.putText(img,'l1',(x+w, y+h), font, 1,(0,255,0),1,cv2.LINE_AA)
-
-# show image with rectangular             
-cv2.imshow('img',img)
-cv2.waitKey(10000)
-cv2.destroyAllWindows()
+	model = loadVGG()
+	img,gray = read2Gray(str(argv[0]))
+	faces = detectFaces(face_cascade,gray)
 
 
+	for (x,y,w,h) in faces:
+		numFaces += 1
+		cropFace,saveFName = saveFaceImg(x,y,w,h,numFaces,img,newpath)
+		if (os.stat(saveFName).st_size) > 0:
+			i_w,i_h = Image.open(saveFName).size
+			if 1.2 >i_w/i_h > 0.8:
+				sex = estSex(saveFName,model)
+				labelFaces(x,y,w,h,img,sex,font)
+		else:
+			os.remove(saveFName)
+		
 
+	# show image with rectangular             
+	cv2.imshow('img',img)
+	cv2.waitKey(10000)
+	cv2.destroyAllWindows()
 
+if __name__ == "__main__":
+   main(sys.argv[1:])
 
-# model = loadVGG(modelPath)
-
-
-model = loadVGG()
