fca29bdfcf740609bfccc2b7f5e92fc082c097dc
Dingchao Zhang <dingchaozhang@g.harvard.edu>
2017-05-17 09:34:23 -0500

    removed not needed files and renamed a few

--- faceRecUtil.py
+++ faceRecUtil.py
@@ -4,24 +4,27 @@ import face_recognition
 
 # Load a sample picture and learn how to recognize it.
 
-dingchao_image = face_recognition.load_image_file("Mike.png")
-dingchao_face_encoding = face_recognition.face_encodings(dingchao_image)[0]
-candy_image = face_recognition.load_image_file("biden1.jpg")
-candy_face_encoding = face_recognition.face_encodings(candy_image)[0]
+kface1_image = face_recognition.load_image_file("Mike.png")
+kface1_face_encoding = face_recognition.face_encodings(kface1_image)[0]
+kface2_image = face_recognition.load_image_file("biden1.jpg")
+kface2_face_encoding = face_recognition.face_encodings(kface2_image)[0]
+kface3_image = face_recognition.load_image_file("profile.jpg")
+kface3_face_encoding = face_recognition.face_encodings(kface3_image)[0]
 
 known_faces = [
-    dingchao_face_encoding,
-    candy_face_encoding
+    kface1_face_encoding,
+    kface2_face_encoding,
+    kface3_face_encoding
 ]
 
-know_faces_names = ['Mike','Biden']
+know_faces_names = ['Mike','Biden','Dingchao']
 
 # Initialize some variables
 face_locations = []
 face_encodings = []
 face_names = []
-known_face_genders = ['?','male']
-known_face_genders_mtli = [[],[]]
+known_face_genders = ['?','male','male']
+known_face_genders_mtli = [[],[],[]]
 current_face_genders = []
 face_landmarks = []
 process_this_frame = True
