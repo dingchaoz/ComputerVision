1b1c9f1945532e28d7a0af8eaacb95e022e4d516
Dingchao Zhang <dingchaozhang@g.harvard.edu>
2017-05-17 19:35:04 -0500

    added Jon Farney's img

--- faceRecUtil.py
+++ faceRecUtil.py
@@ -8,23 +8,26 @@ kface1_image = face_recognition.load_image_file("stiles.jpg")
 kface1_face_encoding = face_recognition.face_encodings(kface1_image)[0]
 kface2_image = face_recognition.load_image_file("biden1.jpg")
 kface2_face_encoding = face_recognition.face_encodings(kface2_image)[0]
-kface3_image = face_recognition.load_image_file("profile.jpg")
+kface3_image = face_recognition.load_image_file("JFarney.jpg")
 kface3_face_encoding = face_recognition.face_encodings(kface3_image)[0]
+kface4_image = face_recognition.load_image_file("jeff.png")
+kface4_face_encoding = face_recognition.face_encodings(kface4_image)[0]
 
 known_faces = [
     kface1_face_encoding,
     kface2_face_encoding,
-    kface3_face_encoding
+    kface3_face_encoding,
+    kface4_face_encoding
 ]
 
-know_faces_names = ['Mike','Biden','Dingchao']
+know_faces_names = ['Mike','Biden','Jon','Jeff']
 
 # Initialize some variables
 face_locations = []
 face_encodings = []
 face_names = []
-known_face_genders = ['?','male','male']
-known_face_genders_mtli = [[],[],[]]
+known_face_genders = ['?','male','male','male']
+known_face_genders_mtli = [[],[],[],[]]
 current_face_genders = []
 face_landmarks = []
 process_this_frame = True
